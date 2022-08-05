816215730563edd9a67efe5a04455963d0b83df4
gabriele.guccione@noovle.com <gabriele.guccione@noovle.com>
2022-08-05 17:34:30 +0200

    fix build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  BASE_URL: 'api-url'
+  baseUrl: 'OrdersDemo-TS-best-practices'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  BASE_URL: '../app/api',
+  baseUrl: ''
 };
 
 /*
